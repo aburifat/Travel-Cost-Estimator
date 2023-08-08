d07660d09e1e39d830ebd70cb04e1e9a848ec1ab
aburifat <rifat.cse4.bu@gmail.com>
2023-08-08 17:42:22 +0600

    meta infoadded

---
+++ travel-cost-estimator.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Travel Cost Estimator(Custom Plugin)
+Description: Estimate travel cost based on user choice
+Version: 1.0.0
+Author: Abu Rifat Muhammed Al Hasib
+Author URI: https://aburifat.com
+Plugin URI: https://aburifat.com
+*/
+
+?>
