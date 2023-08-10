d30b284f419f6866b354584f3492d394f4bc77d1
aburifat <rifat.cse4.bu@gmail.com>
2023-08-10 15:45:33 +0600

    value edit delete option added

--- admin/menu-page.php
+++ admin/menu-page.php
@@ -92,6 +92,72 @@ function get_field_list(){
 	<?php
 }
 
+
+function get_value_list($field_id){
+	global $wpdb;
+    $table_name = $wpdb->prefix . "eg_tce_values";
+
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['add_value'])) {
+            $text = sanitize_text_field($_POST['text']);
+            $image = sanitize_text_field($_POST['image']);
+            $price = intval($_POST['price']);
+
+            $wpdb->insert(
+                $table_name,
+                array(
+					'field_id' => $field_id,
+                    'text' => $text,
+                    'image' => $image,
+                    'price' => $price,
+                )
+            );
+        }
+    }
+	?>
+	<div class="wrap">
+        <h2>Estimator Field Values [Field ID: <?php echo $field_id; ?>]</h2>
+        <h3>Add New Value</h3>
+        <form method="POST">
+            <label>Text: <input type="text" name="text"></label><br>
+			<label>Image: <input type="text" name="image"></label><br>
+			<label>price: <input type="number" name="price"></label><br>
+            <input type="submit" name="add_value" value="Add Field">
+        </form>
+
+        <h3>Values</h3>
+        <table class="wp-list-table widefat striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+					<th>Field ID</th>
+                    <th>Text</th>
+					<th>Image</th>
+					<th>Price</th>
+					<th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $results = $wpdb->get_results("SELECT * FROM $table_name");
+                foreach ($results as $result) {
+					$action_link_base = admin_url('admin.php?page=travel-cost-estimator-menu&field_id=' . $field_id . '&value_id=' . $result->id . '&action=');
+                    echo '<tr>';
+                    echo '<td>' . $result->id . '</td>';
+					echo '<td>' . $result->field_id . '</td>';
+                    echo '<td>' . esc_html($result->text) . '</td>';
+					echo '<td>' . $result->image . '</td>';
+					echo '<td>' . $result->price . '</td>';
+					echo '<td><a href="' . $action_link_base . "1" . '">Edit Value</a> | <a href="' . $action_link_base . '2" onclick="return confirm(\'Are you sure you want to delete this value?\')">Delete</a>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+	<?php
+}
+
 function get_edit_field($field_id){
 	global $wpdb;
     $table_name = $wpdb->prefix . "eg_tce_fields";
@@ -149,9 +215,55 @@ function get_edit_field($field_id){
 	<?php
 }
 
-function get_edit_values($field_id){
-	echo "Edit Value Page";
+
+function get_edit_value($value_id,$field_id){
+	global $wpdb;
+    $table_name = $wpdb->prefix . "eg_tce_values";
+
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['update_value'])) {
+
+			$text = sanitize_text_field($_POST['text']);
+            $image = sanitize_text_field($_POST['image']);
+            $price = intval($_POST['price']);
+
+            $wpdb->update(
+                $table_name,
+                array(
+                    'text' => $text,
+                    'image' => $image,
+                    'price' => $price,
+				),
+				array('id' => $value_id)
+            );
+        }
+    }else{
+		$field = $wpdb->get_row(
+			$wpdb->prepare("SELECT * FROM $table_name WHERE id = %d", $value_id)
+		);
+		if($field){
+			$text = $field->text;
+			$image = $field->image;
+			$price = $field->price;
+		}
+		
+	}
+	?>
+	<div class="wrap">
+		<h2>Estimator Field Values [Field ID: <?php echo $field_id; ?>]</h2>
+        <h3>Update Value</h3>
+        <form method="POST">
+            <label>Field ID: <input type="number" name="field_id" value="<?php echo $field_id; ?>" readonly></label><br>
+            <label>Text: <input type="text" name="text" value="<?php echo $text; ?>"></label><br>
+			<label>Image: <input type="text" name="image" value="<?php echo $image; ?>"></label><br>
+			<label>Price: <input type="number" name="price" value="<?php echo $price; ?>"></label><br>
+            <input type="submit" name="update_value" value="Update Value">
+        </form>
+    </div>
+	<?php
 }
+
+
 
 function delete_field($field_id){
 	global $wpdb;
@@ -161,22 +273,40 @@ function delete_field($field_id){
 	$wpdb->delete($fields_table, array('id' => $field_id));
 }
 
+function delete_value($value_id){
+	global $wpdb;
+	$values_table = $wpdb->prefix . "eg_tce_values";
+	$wpdb->delete($values_table, array('id' => $value_id));
+}
+
 
 
 function render_travel_cost_estimator() {
 	
 	$field_id = isset($_GET['field_id']) ? intval($_GET['field_id']) : 0;
+	$value_id = isset($_GET['value_id']) ? intval($_GET['value_id']) : 0;
 	$action = isset($_GET['action']) ? intval($_GET['action']) : 0;
 
-	if($field_id === 0){
-		get_field_list();
-	}else if($action === 1){
-		get_edit_field($field_id);
-	}else if($action === 2){
-		get_edit_values($field_id);
-	}else if($action === 3){
-		delete_field($field_id);
-		get_field_list();
+	if($value_id != 0 && $field_id != 0){
+		if($action == 1){
+			get_edit_value($value_id,$field_id);
+		}else if($action == 2){
+			delete_value($value_id);
+			get_value_list($field_id);
+		}else{
+			get_value_list($field_id);
+		}
+	}else if($field_id != 0){
+		if($action == 1){
+			get_edit_field($field_id);
+		}else if($action == 2){
+			get_value_list($field_id);
+		}else if($action == 3){
+			delete_field($field_id);
+			get_field_list();
+		}else{
+			get_field_list();
+		}
 	}else{
 		get_field_list();
 	}
